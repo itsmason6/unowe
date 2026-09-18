5d5c108822373baf29c816b37d8947e340adb712
itsmason6 <itsmason6@gmail.com>
2026-09-18 05:19:32 +0100

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-/* Empty = use this same website for sockets (local Node).
-   On Vercel, set this to your Railway/Fly socket host, no trailing slash.
-   Example: "wss://unowe-server.up.railway.app" */
-window.UNOWE_SOCKET = "";
